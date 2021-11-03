08bc74e8b8d38c08f6d15d23e2550fcc7cb463d1
nJavid11 <tu201803071@code.edu.az>
2021-11-03 21:32:01 +0400

    The Task

---
+++ Ilk project/Ikinci project/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ikinci_project
+{
+    public class Class1
+    {
+        public void Aa()
+        {
+            Console.WriteLine("Public");
+        }
+        private void Ee()
+        {
+            Console.WriteLine("Private");
+        }
+
+        protected void Dd()
+        {
+            Console.WriteLine("Protected");
+        }
+
+        internal void Bb()
+        {
+            Console.WriteLine("Internal");
+        }
+
+        protected internal void Cc()
+        {
+            Console.WriteLine("Protected Internal");
+        }
+        protected private void Ff() {
+            Console.WriteLine("Private Protected");
+        }
+
+    }
+}
---
+++ Ilk project/Ilk project/Program.cs
@@ -0,0 +1,24 @@
+﻿using Ikinci_project;
+using System;
+
+namespace Ilk_project
+{
+    class Program : Class1
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Class1 c = new Class1();
+
+            c.Aa();  //public
+            //c.Ee()  error : private
+            //c.Bb()  error : internal
+            p.Cc();  //protected internal
+            //c.Dd()  error : protected
+            //c.Ee()  error : private
+            //c.Ff()  error : private protected
+
+
+        }
+    }
+}
